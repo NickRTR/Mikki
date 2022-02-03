96945e3637ee2893903e495d6b8eac896c3600be
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-02-03 18:08:34 +0100

    Prepared API connection

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+    export async function load() {
+        // const res = await fetch("https://x.glowman554.gq/api/v2/wiki/page/"); // real api
+        const res = await fetch("https://api.adviceslip.com/advice"); // placeholer
+        const data = await res.json();
+        
+        if (res.ok) {
+            return {
+                props: {
+                    data: data.slip.advice
+                }
+            }
+        }
+
+        return {
+            error: new Error("Could not fetch the guides")
+        }
+    }
+</script>
+
+<script>
+    export let data;
+</script>
+
+<body>
+    <p>{data}</p>
+</body>
