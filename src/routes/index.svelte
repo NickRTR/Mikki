631bf1565bece4af7ffb6e251a8fa9e1fe3dcf60
Glowman554 <glowman434@gmail.com>
2022-02-04 21:23:17 +0100

    started changelog

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,13 @@
 <script context="module">
-    import { wiki_list } from "$lib/api.js";
+    import { wiki_list, wiki_changelog } from "$lib/api.js";
 
     export async function load() {
         const res = await wiki_list();
+		const changelog = await wiki_changelog();
         return {
             props: {
-                data: res // array of objects containing page_id, page_title, page_created and page_edited
+                data: res, // array of objects containing page_id, page_title, page_created and page_edited
+				changelog: changelog
             }
         }
     }
@@ -13,11 +15,12 @@
 
 <script>
     import {wiki_get} from "$lib/api.js";
-    import { copyToClipboard } from "$lib/helper";
+    import { copyToClipboard, dateToString } from "$lib/helper";
     import SvelteMarkdown from "svelte-markdown";
     import {slide} from "svelte/transition";
 
     export let data;
+	export let changelog;
     let oldData = data;
 
     let searchInput = "";
@@ -51,6 +54,8 @@
             }
         }
     }
+
+	let show_changelog = false;
 </script>
 
 <body> 
@@ -74,6 +79,25 @@
     {:else}
         <p>Keine passenden Einträge vorhanden</p>
     {/each}
+
+	<div class="changelog_list">
+		<!-- TODO: fix the ▶︎ -->
+		<input type="checkbox" class="toggle" id="changelog_toggle" on:change={() => {show_changelog = !show_changelog}}>
+		<label for="changelog_toggle">▶︎</label>
+		<a id="changelog">Wiki changes</a>
+
+		<!-- TODO: fix the style i guess -->
+
+		{#if show_changelog}			
+			<ul in:slide out:slide>
+				{#each changelog as entry}
+					<li>
+						<p>{entry.what} at {dateToString(entry.when)}</p>
+					</li>
+				{/each}
+			</ul>
+		{/if}
+	</div>
 </body>
 
 <style>
@@ -93,6 +117,24 @@
         border: none;
         font-size: 1.5rem;
     }
+
+	.changelog_list {
+        background-color: var(--light-background);
+        border-radius: .5rem;
+		margin: 1rem 0;
+        padding: .8rem 0;
+	}
+
+	.changelog_list li {
+		list-style: none;
+        display: flex;
+	}
+
+	.changelog_list a {
+		text-decoration: none;
+        color: white;
+	}
+
 
 	.wiki_post_urls {
         display: flex;
